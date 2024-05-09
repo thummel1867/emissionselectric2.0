92f4f27c7cb214dd88c6c37412d83799a58642c3
thummel1867 <thummel1867@gmail.com>
2024-05-09 17:55:37 -0400

    work oon svg map

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import "./App.css";
 import About from "./About";
 import Generation from "./Generation";
 import Ratio from "./Ratio";
+import Map from "./Map";
 
 const App = () => {
   const [country, setCountry] = useState();
@@ -26,6 +27,7 @@ const App = () => {
           <h1>Countries</h1>
         </Link>
       </nav>
+      {<Map />}
       <main className="main">
         <Routes>
           <Route path="/" element={<Home />} />
@@ -50,34 +52,3 @@ const App = () => {
 };
 
 export default App;
-
-//pull the country list from the api ->
-//map through that so that when a button is clicked, every country will display with a link
-
-//each one of those links should bring up information about the country - bringing the carbon emissions associated with the following:
-//coal
-//gas
-//geothermal
-//hydro
-//nuclear
-//petroleum
-//solar photovoltaic
-//Wind
-//will have to use state to grab the name of the region and plug it into the fetch request (should there be a component for each type of electricity generation?)
-
-//questions for Jay:
-//mapping through countries list - use Link to generate a link for each?
-//how to manipulate fetch POST request with state
-
-//stretch goal - render a graph on each countries page
-//there will also be a search bar that will transport the user to the correct page
-//give the user the option to choose two different countries and show their data side by side
-//highlight comparison between countries through color, or other differentiation
-
-//styling
-
-
-//Thursday: 
-//readme
-//deployment
-//write to API
